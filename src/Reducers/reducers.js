75096272a9b00792e73edae610f0b870d10401c4
John Kim <joimkim@gmail.com>
2016-10-30 09:17:00 -0700

    Donut mode

--- src/Reducers/reducers.js
+++ src/Reducers/reducers.js
@@ -4,7 +4,7 @@ import { combineReducers } from 'redux';
 const initialState = {
 	ModeFilters: 'SHOW_ALL',
 	data: data.transactions,
-	visibleData: data.transactions,
+	visibleData: {}
 }
 
 const visibilityFilter = (state = initialState, action) => {  
@@ -21,8 +21,8 @@ const visibilityFilter = (state = initialState, action) => {
 }
 
 const ignoreDonuts = (state) => {
-	let visibleData = state.data.filter(isNotDonut)
-	return Object.assign([], state, visibleData: visibleData)
+	let visibleData = monthYearSplitAndSum(state.data.filter(isNotDonut))
+	return visibleData
 }
 
 function isNotDonut(transaction) {
@@ -31,6 +31,37 @@ function isNotDonut(transaction) {
 	} else {
 		return false
 	}
+}
+
+function monthYearSplitAndSum(visibleData) {
+	let totalDebit = 0
+	let totalCredit = 0
+	const monthYearSplitAndSum = {}
+	
+	console.log(visibleData)
+	// Create sum for debit and credit. Also create month & year parser. Doing both work here instead of
+	// iterating through again.
+	visibleData.forEach((transaction) => {
+	// Create date parser. Rather than converting to date.
+	// split 2014-10-08T10:41:00.000Z by -. first 2 index gives unquie year and month key
+	// that can be stored to the collection monthYearSplitAndSum
+	const date = transaction['transaction-time'].split('-')
+	const yearMonth = (date[0] + date[1])
+		if (monthYearSplitAndSum[yearMonth]) {
+			monthYearSplitAndSum[yearMonth].push(transaction)
+		} else {
+			monthYearSplitAndSum[yearMonth] = [transaction]
+		}
+
+	// Logic to push transaction amount to debit or credit
+		if (transaction.amount <= 0) {
+			totalDebit += transaction.amount
+		} else {
+			totalCredit += transaction.amount
+		}
+	})
+
+	return visibleData = {monthYearSplitAndSum, totalDebit, totalCredit}
 }
 
 const rootReducers = combineReducers({

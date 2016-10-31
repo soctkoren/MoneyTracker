eae00554c3272414b0eaf0962bb7a121a98c073a
John Kim <joimkim@gmail.com>
2016-10-31 11:31:30 -0700

    add Thunk middleware

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise';
+import thunkMiddleware from 'redux-thunk';
 import rootReducers from './Reducers/reducers.js';
 import TransactionActivityPage from './Components/TransactionActivityPage';
 import './index.css';
 
 //Configure middleware w/ redux-promise for AJAX requests
 const createStoreWithMiddleware = applyMiddleware(
-  promise
+  promise,
+  thunkMiddleware
 )(createStore);
 
 const store = createStore(rootReducers)
